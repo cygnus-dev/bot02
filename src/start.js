3fb5cb868daaa6e685d532e3dc9da510bb3d66f2
cygnus-dev <68337678+cygnus-dev@users.noreply.github.com>
2020-11-18 21:47:49 +0100

    added cmd start.js

--- src/start.js
+++ src/start.js
@@ -1,10 +1,10 @@
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// client.login('NzYxNjI4MDY2MjA3NzYwNDI0.X3dXaQ.a-IyF7sSj8-LdFvWjb6ZK6I_jAI');
+Discord.p
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,11 +16,33 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', msg => {
+  if (msg.content === 'joke') {
+    var joke_list = ["homoeass", "humus", "js", "pytho god"];
+    var joke = joke_list[Math.floor(Math.random() * joke_list.length)];  
+    msg.reply(joke);
+  }
+});
+
+client.on('message', message => {
+  const swearWords = ["cock", "shit", "fuck", "js", "gay", "minecraft", "ziad", "why", "piss", "british", "Fuck"];
+  if( swearWords.some(word => message.content.includes(word)))  {
+    message.reply("ohno you said notok work!?!11!");
+  }
+})
+
 client.on('message', msg =>{
   if (msg.content === 'monke') {
     msg.reply('monke!');
     }
 });
 
-client.login('NzYxNjI4MDY2MjA3NzYwNDI0.X3dXaQ.ZytBVdIOjFnhC-npkRDzEfm7Zwk');
-// client.login(process.env.DISCORDJS_TOKEN);
+
+
+
+// client.on('message', msg =>{
+//   if (msg.con)
+// });
+
+// client.login('NzYxNjI4MDY2MjA3NzYwNDI0.X3dXaQ.ti5HMovLZVUQ-QFvl1Dggxx-LGE');
+client.login(process.env.DISCORDJS_TOKEN);
